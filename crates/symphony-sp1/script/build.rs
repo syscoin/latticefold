0be6507320aea945d01e3ae5f50b1cfa487dc080
jagdeep sidhu <sidhujag@syscoin.org>
2026-01-04 23:30:14 -0800

    rm crate

--- crates/symphony-sp1/script/build.rs
+++
@@ -1,8 +0,0 @@
-fn main() {
-    // Build the zkVM guest program and make its ELF available via `include_elf!(...)`.
-    //
-    // NOTE: This is scaffolding. The guest currently only proves a trivial computation,
-    // and will be wired to the Symphony Π_fold verifier next.
-    sp1_build::build_program("../program");
-}
-
